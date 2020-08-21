4e7446cbeac38ca5b190cf99dab6ae01baf9f4df
sri2317 <srikanthuk@yahoo.com>
2020-08-21 10:29:05 +0100

    lamda project

---
+++ lamdatest01/src/main/java/lamdatest/lamdatest01/App.java
@@ -0,0 +1,17 @@
+package lamdatest.lamdatest01;
+
+import org.testng.annotations.Test;
+
+/**
+ * Hello world!
+ *
+ */
+@Test
+public class App 
+{
+    //public static void main( String[] args )
+    public static void test1()
+	{
+        System.out.println( "Hello World!" );
+    }
+}
